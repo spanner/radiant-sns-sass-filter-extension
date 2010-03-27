7f31d28d4b25585fd5c937b50b9c1d0fdb303f9a
spanner <will@spanner.org>
2010-03-27 13:30:53 +0000

    SnS.sass_directory config item sets path for sass includes

--- lib/sns_sass_filter.rb
+++ lib/sns_sass_filter.rb
@@ -8,7 +8,8 @@ class SnsSassFilter < StylesheetFilter
     # just make sure you know the consequences of doing so (including
     # that you may need to update all your stylesheets to yield a new
     # LAST-MODIFIED date from the cache.
-    @sass_options = {}
+    load_path = Radiant::Config['SnS.sass_directory'] || 'css/sass/'
+    @sass_options = {:load_paths => [File.join(RAILS_ROOT, 'public', load_path)]}
   end
 
 
